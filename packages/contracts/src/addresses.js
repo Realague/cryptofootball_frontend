825c74d5a761fc57a50b8553ca569688f498b546
Realague <realague@gmail.com>
2022-01-22 16:05:38 +0100

    [~] Game contract address

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -5,9 +5,17 @@ const addresses = {
 	BUSD: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
 	GBTOKEN: '0x9c919E7b3cE2623F4725ab4b919eAb737f6Cc176',
 	Marketplace: '0xBAfC64a490623c46273a11bA04b438777C7B7450',
-	Game: '0x2287cB6f14181B55c31ddFdB84f08aa4985d463F',
+	Game: '0x3BF586ea30990E54843F0D3862347F59B5685fED',
 	FootballPlayers: '0xd910577c16ABE09830C75d35Db17bb3CcF9825aD',//0x570D978BFf3B7dDc6eEBbCaf72Be9e8ACcaC0CD1
 	FootballHeroesStorage: '0x10b7160e9e6c6dd6B7DfeA4dFddF4ACfC3E5a6E3'
 }
+/*
+[0, 0, 3]
+[[0, 1, 3], [0, 1, 3], [0, 1, 3], [0, 1, 3]]
+[[0, 2, 3], [0, 2, 3], [0, 2, 3], [0, 2, 3]]
+[[0, 3, 3],[0, 3, 3]]
 
+[0,"0",3],[[0,"1",3],[0,"1",3],[0,"1",3],[0,"1",3]],[[0,"2",3],[0,"2",3],[0,"2",3],[0,"2",3]],[[0,"3",3],[0,"3",3]],"100","80"
+[[2,4,4]]
+ */
 export default addresses

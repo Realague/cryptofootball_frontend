367db8eb57f917b8b7c7e1f565c29b98079fa84a
Realague <realague@gmail.com>
2022-02-01 00:29:24 +0100

    [+] new mint and training

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -5,15 +5,11 @@ const addresses = {
 	BUSD: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
 	GBTOKEN: '0x4b900990729e30261c3102b0557fd34219d90366',
 	Marketplace: '0xaBb5F8d120fAeddc546494F59fE9085D3ac0b0C4',
-	Game: '0x1580512AF3AD0E56dBca41d6CB61dd9EA9E5b019',
-	FootballPlayers: '0x02A956665bEf56dF602259AC1e29757794dB6d0a',//0x570D978BFf3B7dDc6eEBbCaf72Be9e8ACcaC0CD1
+	Game: '0x4c084bC2699C8f532bCd2948B1BA5A4a3a61de2c',
+	FootballPlayers: '0xE66987DDcbE467BEfe16292c15a6c9AC0972d098',
 	FootballHeroesStorage: '0x4aD98cFbA873e475D69Fea4093Ad354a0E8D1C3C'
 }
 /*
-[0, 0, 3]
-[[0, 1, 3], [0, 1, 3], [0, 1, 3], [0, 1, 3]]
-[[0, 2, 3], [0, 2, 3], [0, 2, 3], [0, 2, 3]]
-[[0, 3, 3],[0, 3, 3]]
 
 [0,"0",3, 4],[[0,"1",3, 4],[0,"1",3,4],[0,"1",3,4],[0,"1",3,4]],[[0,"2",3,4],[0,"2",3,4],[0,"2",3,4],[0,"2",3,4]],[[0,"3",3,4],[0,"3",3,4]],"100","80"
 [[2,4,4]]

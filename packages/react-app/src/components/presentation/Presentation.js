9bc2101161536751ba50111586026c188f06ab5a
barroso_k <kbarroso68@gmail.com>
2022-02-07 21:32:05 +0100

    Updated presentation page

--- packages/react-app/src/components/presentation/Presentation.js
+++ packages/react-app/src/components/presentation/Presentation.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import '../../css/presentation.css'
-import { Box, Divider, Stack, Typography } from '@mui/material'
+import { Box, Divider, Stack, Typography, useMediaQuery } from '@mui/material'
+import TokenImage from '../../images/token.png'
+import theme from '../../theme'
+import { FitnessCenter } from '@mui/icons-material'
 
 
 function Presentation() {
+	const isMobile = useMediaQuery(theme.breakpoints.down('lg'))
+
+	const boxStyle = (color) => ({
+		borderRadius: '5px',
+		boxShadow: `0 0 10px ${color}`,
+		borderTop: `3px solid ${color}`,
+		height: '250px',
+		width: '375px',
+		justifyContent: 'space-between',
+		p: 2,
+		m: 2,
+	})
+
 	return (
 		<Stack alignItems="center" spacing={2} p={2}>
 			<Typography variant="h4" textAlign="center">Play to earn NFT</Typography>
@@ -14,31 +30,31 @@ function Presentation() {
                     Train them by taking penalties.
 			</Typography>
 			<div className="row1-container">
-				<div className="box box-down cyan">
-					<h2>Recruit your players</h2>
-					<img src="https://assets.codepen.io/2301174/icon-supervisor.svg" alt=""/>
-				</div>
+				<Stack sx={boxStyle('red')} spacing={2}>
+					<Typography variant="h4">Recruit your players</Typography>
+					<Divider flexItem variant="middle" />
+					<img src="/demo-card.png" style={{ width: '64px', height: '100px', alignSelf: 'flex-end' }}/>
+				</Stack>
 
-				<div className="box red">
-					<h2>Train your players</h2>
-					<img src="https://assets.codepen.io/2301174/icon-team-builder.svg" alt=""/>
-				</div>
+				<Stack sx={boxStyle(theme.palette.secondary.main)} spacing={2}>
+					<Typography variant="h4">Play with your players</Typography>
+					<Divider flexItem variant="middle" />
+					<img src="/stadium.png" style={{ width: '128px', height: '75px', alignSelf: 'flex-end' }} alt=""/>
+				</Stack>
 
-				<div className="box box-down blue">
-					<h2>Boost your players</h2>
-					<img src="https://assets.codepen.io/2301174/icon-calculator.svg" alt=""/>
-				</div>
+				<Stack sx={boxStyle(theme.palette.primary.light)} spacing={2}>
+					<Typography variant="h4">Train your players</Typography>
+					<Divider flexItem variant="middle" />
+					<img src="/coupetrans.png" style={{ width: '64px', height: '80px', alignSelf: 'flex-end' }} alt=""/>
+				</Stack>
 			</div>
-			<Box sx={{
-				width: '100%',
-				display: 'flex',
-				justifyContent: 'center',
-			}}>
-				<div className="box orange">
-					<h2>Win tokens</h2>
-					<img src="https://assets.codepen.io/2301174/icon-karma.svg" alt=""/>
-				</div>
-			</Box>
+			<div className="row2-container">
+				<Stack sx={boxStyle('green')} spacing={2}>
+					<Typography variant="h4">Earn our tokens</Typography>
+					<Divider flexItem variant="middle" />
+					<img style={{ width: '64px', height: '64px', alignSelf: 'flex-end' }} src={TokenImage} alt="token"/>
+				</Stack>
+			</div>
 		</Stack>
 	)
 }

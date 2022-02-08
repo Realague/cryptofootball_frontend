9798a67086946a2e787f9146f1e8a5cbe9a8d1ce
barroso_k <kbarroso68@gmail.com>
2022-02-08 23:52:00 +0100

    FIx

--- packages/react-app/src/layout/teamDrawer/components/DrawerContent.js
+++ packages/react-app/src/layout/teamDrawer/components/DrawerContent.js
@@ -23,12 +23,6 @@ const DrawerContent = ({ lastPlayerDropped }) => {
 	const navigate = useNavigate()
 
 
-	const { setIsOpen } = useTour()
-	const steps = [{
-		selector: '.first-step',
-		content: 'This is my first Step',
-	}]
-
 	useEffect(() => {
 		fetch().finally(() => setCompositions(Strategy.Strategies))
 		return () => setCompositions([])
@@ -97,137 +91,135 @@ const DrawerContent = ({ lastPlayerDropped }) => {
 	}
 
 	return (
-		<TourProvider steps={steps}>
-			<Box sx={{
-				flexGrow: 1,
-				width: '100%',
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				justifyContent: 'flex-start'
-			}}>
-				{
-					(collection.length === 0) ?
-						<Stack
-							sx={{
-								height: '100vh',
-								alignItems: 'center',
-								justifyContent: 'center',
-							}}
-							spacing={2}
-							p={2}
-						>
-							<Typography textAlign='center' variant="body2">
+		<Box sx={{
+			flexGrow: 1,
+			width: '100%',
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			justifyContent: 'flex-start'
+		}}>
+			{
+				(collection.length === 0) ?
+					<Stack
+						sx={{
+							height: '100vh',
+							alignItems: 'center',
+							justifyContent: 'center',
+						}}
+						spacing={2}
+						p={2}
+					>
+						<Typography textAlign='center' variant="body2">
 							You don't have any player in your collection
-							</Typography>
-							<Typography textAlign='center'>
+						</Typography>
+						<Typography textAlign='center'>
 							you can open the mint's page by clicking on the button below
-							</Typography>
-							<Button
-								variant="contained"
-								onClick={() => {
-									navigate('/mint')
-								}}
-							>
+						</Typography>
+						<Button
+							variant="contained"
+							onClick={() => {
+								navigate('/mint')
+							}}
+						>
 							Mint player
-							</Button>
-						</Stack>
-						:
-						(team && team.strategy === undefined) ?
-							<Stack>
-								<Typography variant="subtitle1">
+						</Button>
+					</Stack>
+					:
+					(team && team.strategy === undefined) ?
+						<Stack>
+							<Typography variant="subtitle1">
                             You have to select your strategy first
-								</Typography>
-								<Grid container spacing={2} p={2}>
-									{
-										compositions.map(s => (
-											<Grid key={s.id} item>
-												<Button
-													onClick={() => selectStrategy(s)}
-													variant="contained"
-													color="secondary"
-												>
-													{s.name}
-												</Button>
-											</Grid>
-										))
-									}
-								</Grid>
+							</Typography>
+							<Grid container spacing={2} p={2}>
+								{
+									compositions.map(s => (
+										<Grid key={s.id} item>
+											<Button
+												onClick={() => selectStrategy(s)}
+												variant="contained"
+												color="secondary"
+											>
+												{s.name}
+											</Button>
+										</Grid>
+									))
+								}
+							</Grid>
 
-							</Stack>
-							: // strategy selected
-							<Stack alignItems="center" sx={{ width: '100%', backgroundColor: theme.palette.background.valueOf() }} spacing={2}>
-								<Stack direction={isMobile ? 'column' : 'row'} spacing={1} alignItems="center">
-									<Button
-										disabled={team.players.length !== 11}
-										onClick={() => saveTeam()}
-										variant="contained"
-										color="secondary"
-									>
+						</Stack>
+						: // strategy selected
+						<Stack alignItems="center" sx={{ width: '100%', backgroundColor: theme.palette.background.valueOf() }} spacing={2}>
+							<Stack direction={isMobile ? 'column' : 'row'} spacing={1} alignItems="center">
+								<Button
+									disabled={team.players.length !== 11}
+									onClick={() => saveTeam()}
+									variant="contained"
+									color="secondary"
+								>
 								Save team
-									</Button>
-									<Button
-										onClick={() => resetStrategy()}
-										variant="contained"
-										color="primary"
-									>
+								</Button>
+								<Button
+									onClick={() => resetStrategy()}
+									variant="contained"
+									color="primary"
+								>
 								Reset team
-									</Button>
-									<Button
-										disabled={team.players.length === 0}
-										onClick={() => destroyTeam()}
-										variant="contained"
-										color="error"
-									>
+								</Button>
+								<Button
+									disabled={team.players.length === 0}
+									onClick={() => destroyTeam()}
+									variant="contained"
+									color="error"
+								>
 								Disband team
-									</Button>
-								</Stack>
-
-								<Divider variant="middle" flexItem/>
-
-								<Stack spacing={1} sx={{
-									overflowY: 'scroll',
-									width: '100%',
-									height: '80vh',
-								}}>
-									{
-										compositions[team.strategy] !== undefined && Object.keys(compositions[team.strategy].composition).map(role => {
-											return (
-												<React.Fragment key={role}>
-													<Stack key={role} direction="row" spacing={2} alignItems="center">
-														<Typography variant="subtitle2">{Position.Positions[role].name}</Typography>
-														<Typography variant="subtitle2">
-															{team.players.filter(p => p.position === role).length} / {Strategy.Strategies[team.strategy].composition[role]}
-														</Typography>
-													</Stack>
-													{
-														team.players.filter(p => p.position === role).map(p => {
-															return (lastPlayerDropped !== undefined && lastPlayerDropped.id === p.id) ?
-																<Slide appear in key={p.id} direction="right">
-																	<LayoutContent>
-																		<PlayerListItem
-																			player={p}
-																			icon={<Remove/>}
-																			onClick={() => dispatch(removePlayerFromTeamById(p.id))}/>
-																	</LayoutContent>
-																</Slide>
-																:
-																<PlayerListItem
-																	icon={<Remove/>}
-																	onClick={() => dispatch(removePlayerFromTeamById(p.id))}
-																	key={p.id} player={p}/>
-														})
-													}
-												</React.Fragment>
-											)
-										})
-									}
-								</Stack>
+								</Button>
 							</Stack>
 
-				}
-			</Box>
-		</TourProvider>
+							<Divider variant="middle" flexItem/>
+
+							<Stack spacing={1} sx={{
+								overflowY: 'scroll',
+								width: '100%',
+								height: '80vh',
+							}}>
+								{
+									compositions[team.strategy] !== undefined && Object.keys(compositions[team.strategy].composition).map(role => {
+										return (
+											<React.Fragment key={role}>
+												<Stack key={role} direction="row" spacing={2} alignItems="center">
+													<Typography variant="subtitle2">{Position.Positions[role].name}</Typography>
+													<Typography variant="subtitle2">
+														{team.players.filter(p => p.position === role).length} / {Strategy.Strategies[team.strategy].composition[role]}
+													</Typography>
+												</Stack>
+												{
+													team.players.filter(p => p.position === role).map(p => {
+														return (lastPlayerDropped !== undefined && lastPlayerDropped.id === p.id) ?
+															<Slide appear in key={p.id} direction="right">
+																<LayoutContent>
+																	<PlayerListItem
+																		player={p}
+																		icon={<Remove/>}
+																		onClick={() => dispatch(removePlayerFromTeamById(p.id))}/>
+																</LayoutContent>
+															</Slide>
+															:
+															<PlayerListItem
+																icon={<Remove/>}
+																onClick={() => dispatch(removePlayerFromTeamById(p.id))}
+																key={p.id} player={p}/>
+													})
+												}
+											</React.Fragment>
+										)
+									})
+								}
+							</Stack>
+						</Stack>
+
+			}
+		</Box>
 	)
 }
 
